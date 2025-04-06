0351d5a63a50a07ef163ea2cd636f57570c98fcb
yeonjy73 <yeon703@naver.com>
2025-04-06 19:02:44 +0900

    fix: import

--- src/main/java/controller/CreateUserController.java
+++ src/main/java/controller/CreateUserController.java
@@ -1,13 +1,14 @@
 package controller;
 
 import core.db.MemoryUserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jwp.model.User;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebServlet("/user/signup")
--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -1,14 +1,15 @@
 package controller;
 
 import core.db.MemoryUserRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jwp.model.User;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.Collection;
 
